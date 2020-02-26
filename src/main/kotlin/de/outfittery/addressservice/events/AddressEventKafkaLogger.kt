32fea57bd243f37391fd140c3aa1da75894917b5
ricardo.baumann <ricardo.baumann@outfittery.de>
2020-02-26 16:19:13 +0100

    async events

---
+++ src/main/kotlin/de/outfittery/addressservice/events/AddressEventKafkaLogger.kt
@@ -0,0 +1,20 @@
+package de.outfittery.addressservice.events
+
+import mu.KLogging
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
+import org.springframework.transaction.event.TransactionPhase
+import org.springframework.transaction.event.TransactionalEventListener
+
+@Component
+class AddressEventKafkaLogger {
+    
+    companion object : KLogging()
+
+    @Async
+    @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
+    fun on(eventDto: AddressEventDto) {
+        logger.info("Logging event on kafka: {}", eventDto)
+    }
+
+}
