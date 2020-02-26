28a392b908d3b51833d9e5b4efdffd4f33034408
ricardo.baumann <ricardo.baumann@outfittery.de>
2020-02-26 16:17:23 +0100

    async events

--- src/main/kotlin/de/outfittery/addressservice/events/AddressEventDbLogger.kt
+++ src/main/kotlin/de/outfittery/addressservice/events/AddressEventDbLogger.kt
@@ -3,6 +3,7 @@ package de.outfittery.addressservice.events
 import de.outfittery.addressservice.models.AddressEvent
 import de.outfittery.addressservice.service.AddressEventService
 import mu.KLogging
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Component
 import org.springframework.transaction.event.TransactionPhase
 import org.springframework.transaction.event.TransactionalEventListener
@@ -12,6 +13,7 @@ class AddressEventDbLogger(private val addressEventService: AddressEventService)
 
     companion object : KLogging()
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     fun on(eventDto: AddressEventDto) {
         try {
